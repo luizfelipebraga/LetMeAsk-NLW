59a7cefe79821bfc16395d3a8030ddc7166766c1
Luiz <ruivolfb@gmail.com>
2021-06-24 22:06:27 -0300

    fixing app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 import Home from './pages/Home';
 import NewRoom from './pages/NewRoom';
-import { auth, firebase } from './services/firebase';
 
 import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
   return (
     <BrowserRouter>
+    {/ Englobar o contexto para ter o context do usuario durante todas páginas. /}
       <AuthContextProvider>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/new" component={NewRoom} />
