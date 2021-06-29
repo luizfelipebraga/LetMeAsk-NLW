a772755f4bc7c9b894c9144ba8fefdec69b73f3f
Luiz <ruivolfb@gmail.com>
2021-06-29 20:13:52 -0300

    creating room code component and add a function to copy this code and share with friends :)

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,23 @@
+import copyImg from '../assets/images/copy.svg';
+
+import styles from '../styles/roomCode.module.scss';
+
+type RoomCodeProps = {
+    code: string;
+}
+
+export default function RoomCode(props: RoomCodeProps) {
+
+    function copyRoomCodeToClipboard() {
+        navigator.clipboard.writeText(props.code)
+    }
+
+    return (
+        <button className={styles.roomCode} onClick={copyRoomCodeToClipboard}> 
+            <div>
+                <img src={copyImg} alt="Copiar código" />
+            </div>
+            <span>Sala {props.code}</span>
+        </button>
+    )
+}
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,33 +1,43 @@
 import styles from '../styles/room.module.scss';
 
+import {useParams} from 'react-router-dom';
+
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
+import RoomCode from '../components/RoomCode';
+
+type RoomParams = {
+    id: string
+}
 
 export default function Room() {
+    
+    const params = useParams<RoomParams>();
+    
     return (
         <div className={styles.pageRoom}>
             <header>
                 <div className={styles.content}>
                     <img src={logoImg} alt="Logo LetmeAsk" />
-                    <div>codigo</div>
+                    <RoomCode code={params.id}/>
                 </div>
             </header>
 
-            <main className={styles.content}>
+            <main>
                 <div className={styles.roomTitle}>
                     <h1>Sala React</h1>
                     <span>4 Perguntas</span>
                 </div>
+
+
+                <form>
+                    <textarea placeholder="O que você quer perguntar?"></textarea>
+                    <div className={styles.formFooter}>
+                        <span>Para enviar uma pergunta, <button>Faça seu Login</button></span>
+                        <Button>Enviar Pergunta</Button>
+                    </div>
+                </form>
             </main>
-
-            <form >
-                <textarea placeholder="O que você quer perguntar?"/>
-
-                <div className={styles.formFooter}>
-                    <span>Para enviar uma pergunta, <button>Faça seu Login</button></span>
-                    <Button>Enviar Pergunta</Button>
-                </div>
-            </form>
         </div>
     )
 }
