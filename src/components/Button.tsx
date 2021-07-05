bf2c6187d482e612315d2bac5e3a317f6a6d51e4
Luiz <ruivolfb@gmail.com>
2021-07-04 22:10:52 -0300

    class4 done - learning about useHooks(useRoom) to share with others components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import { ButtonHTMLAttributes } from 'react';
 import styles from '../styles/components/button.module.scss';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-    isOutline: boolean
+    isOutline?: boolean
 };
 
 
